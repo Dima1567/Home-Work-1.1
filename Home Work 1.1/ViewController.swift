e8e495193e45c7e1403e01308775ef6173c06386
Dima1567 <7833288d@gmail.com>
2021-05-18 19:52:24 +0300

    Initial Commit

---
+++ Home Work 1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 1.1
+//
+//  Created by Dima Savitskiy on 18.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
